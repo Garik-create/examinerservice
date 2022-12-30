bb4b698464d89a565ed4423f156e0656aac14aac
Garik-create <pfdfkbybujhm@yandex.ru>
2022-12-30 14:21:58 +0300

    Initial commit.

---
+++ src/main/java/com/skypro/coursework3/examinerservice/ExaminerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.coursework3.examinerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminerserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExaminerserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/coursework3/examinerservice/ExaminerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.coursework3.examinerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminerserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
